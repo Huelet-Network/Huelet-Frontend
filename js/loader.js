12247e5fb89c409045965c3598851ad9aafffa7d
Levi Richardson <litbelb1@outlook.com>
2021-01-20 11:57:41 -0800

    add js loader

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+ function loaderFn() {
+            var loader = document.getElementById("loader");
+            var cssloader = document.getElementById("css-loader");
+            loader.remove();
+            cssloader.remove();
+        }
